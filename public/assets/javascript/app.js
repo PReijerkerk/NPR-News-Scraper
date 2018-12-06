8c8c436e35bf00ea53d86f9d62af74d7367a5a25
P Reijerkerk <preijerkerk@gmail.com>
2018-12-05 19:56:01 -0500

    Bug fixes for es6 template literals

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,18 +1,18 @@
 $(()=>{
     //declare all functions
     const scrapeArticle = ()=>{
-        $.get('/scrape')
+        $.get("/scrape")
         .then((data)=>{
-            $('body').html(data);
+            $("body").html(data);
         });
     };
 
     const saveArticle = function() {
-        let id = $(this).data('id');
+        let id = $(this).data("id");
 
         $.ajax({
-            url: '/article/${id}',
-            method: 'PUT'
+            url: `/article/${id}`,
+            method: "PUT"
         })
         .then((data)=>{
             location.reload();
@@ -20,119 +20,119 @@ $(()=>{
     };
 
     const removeArticle = function() {
-        let id = $(this).data('id');
+        let id = $(this).data("id");
 
         $.ajax({
-            url: '/article/remove/${id}',
-            method: 'PUT'
+            url: "/article/remove/${id}",
+            method: "PUT"
         })
         .then((data)=> {
             location.reload();
         });
     };
 
-    // const viewComments = function() {
-    //     let articleId = $(this).data('id');
+    const viewComments = function() {
+        let articleId = $(this).data("id");
 
-    // //Send request to grab the articles comments
-    //     $.ajax({
-    //         url: '/article/${articleId}',
-    //         method: 'GET'
-    //     })
-    //     .then((data)=> {
-    //         `$('.modal-content').html(
-    //         <div class="modal-header">
-    //             <h4 class="modal-title">${data.title}</h4>
-    //             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-    //             <span aria-hidden="true">&times;</span>
-    //             </button>)
-    //         </div>
-    //         <div class="modal-body">
-    //             <ul class="list-group"></ul>
-    //             <textarea name="comment" class="comment-content"></textarea>
-    //         </div>
-    //         <div class="modal-footer">
-    //             <button type="button" data-id="${data._id}" class="btn btn-primary btn-save-comment">Save Comment</button>
-    //             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-    //         </div>
-    //         )`;
+    //Send request to grab the articles comments
+        $.ajax({
+            url: "/article/${articleId}",
+            method: "GET"
+        })
+        .then((data)=> {
+            `$('.modal-content').html(
+            <div class="modal-header">
+                <h4 class="modal-title">${data.title}</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>)
+            </div>
+            <div class="modal-body">
+                <ul class="list-group"></ul>
+                <textarea name="comment" class="comment-content"></textarea>
+            </div>
+            <div class="modal-footer">
+                <button type="button" data-id="${data._id}" class="btn btn-primary btn-save-comment">Save Comment</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+            )`;
 
-    //         let totalComments = data.comments.length;
+            let totalComments = data.comments.length;
 
-    //         //If there are no comments
-    //         if (totalComments == 0) {
-    //             let message = '<small class="text-muted">This article does not have any comments yet.</small>';
-    //             $('.modal-body').prepend(message);
-    //         }
-    //         //If comments exist
-    //         else {
-    //             let comments = data.comments;
-    //             //loops through comments and appends to modal
-    //             comments.forEach(comment =>{
-    //                 `$('.list-group').append(
-    //                 <li class="list-group-item justify-content-between">
-    //                     ${comment.body}
-    //                     <span><i class="material-icons" data-id="${comment.id}">delete_forever</i></span>
-    //                 </li>
-    //                 )`;
-    //             });
-    //         }
+            //If there are no comments
+            if (totalComments == 0) {
+                let message = `<small class="text-muted">This article does not have any comments yet.</small>`;
+                $(".modal-body").prepend(message);
+            }
+            //If comments exist
+            else {
+                let comments = data.comments;
+                //loops through comments and appends to modal
+                comments.forEach(comment =>{
+                    $(".list-group").append(
+                    `<li class="list-group-item justify-content-between">
+                        ${comment.body}
+                        <span><i class="material-icons" data-id="${comment.id}">delete_forever</i></span>
+                    </li>`
+                    );
+                });
+            }
 
-    //         $('.modal').modal('show');
-    //     });
-    // };
+            $(".modal").modal("show");
+        });
+    };
 
     const saveComment = function() {
-        let id = $(this).data('id');
-        let content = $('.comment-content').val().trim();
+        let id = $(this).data("id");
+        let content = $(".comment-content").val().trim();
 
         if (content) {
             $.ajax({
-                url: '/comment/${id}',
-                method: 'POST',
+                url: "/comment/${id}",
+                method: "POST",
                 data: {body: content}
             })
             .then((data)=>{
                 //clears comment field
-                $('.comment-content').val('');
+                $(".comment-content").val("");
                 //Hide modal
-                $('.modal').modal('hide');
+                $(".modal").modal("hide");
             });
         }
         else {
-            $('.comment-content').val('');
+            $(".comment-content").val("");
             return;
         }
     };
 
     const deleteComment = function() {
-        let id = $(this).data('id');
+        let id = $(this).data("id");
 
         $.ajax({
-            url: '/comment/${id}',
-            method: 'DELETE'
+            url: "/comment/${id}",
+            method: "DELETE"
         })
         .then((data)=>{
-            $('.modal').modal('hide');
+            $(".modal").modal("hide");
         });
     };
 
     //Hides the scrape button if on the 'saved' page
-    if (window.location.href.includes('saved')) {
-        $('.scrape').hide();
+    if (window.location.href.includes("saved")) {
+        $(".scrape").hide();
     }
 
     //Scrollbar fix
-    const contentBox = $('.comment-content');
+    const contentBox = $(".comment-content");
     contentBox.scrollTop = contentBox.scrollHeight;
 
     //click events
-    $('.scrape').on('click', scrapeArticle);
-    $('.btn-save').on('click', saveArticle);
-    $('.btn-remove').on('click', removeArticle);
-    //$('.btn-view-comments').on('click', viewComments);
+    $(".scrape").on("click", scrapeArticle);
+    $(".btn-save").on("click", saveArticle);
+    $(".btn-remove").on("click", removeArticle);
+    $(".btn-view-comments").on("click", viewComments);
 
     //Click events for dynamically created elements
-    $(document).on('click', '.btn-save-comment', saveComment);
-    $(document).on('click', '.material-icons', deleteComment);
+    $(document).on("click", ".btn-save-comment", saveComment);
+    $(document).on("click", ".material-icons", deleteComment);
 });
